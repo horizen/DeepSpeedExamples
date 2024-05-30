3d6a1031af2bdefe883c0e9b5b9ba33653d6ad37
willyao <willyao@tencent.com>
2024-05-30 11:07:19 +0800

    add benchmark

--- benchmarks/training/run_synthetic_benchmark.sh
+++ benchmarks/training/run_synthetic_benchmark.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-torchrun --nnodes=$NODE_NUM --nproc-per-node=$GPU_NUM_PER_NODE --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT synthetic_benchmark.py --model resnet50 --num-iters 10
+dlprof --mode pytorch torchrun --nnodes=$NODE_NUM --nproc-per-node=$GPU_NUM_PER_NODE --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT synthetic_benchmark.py --model resnet50 --num-iters 10
